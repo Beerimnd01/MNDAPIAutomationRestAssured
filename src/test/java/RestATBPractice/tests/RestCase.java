e5998d52397210a02d736b8fe285a7e2a4e8d446
Beerimnd01 <beerimnd01@gmail.com>
2024-02-28 10:44:52 +0530

    first commit

---
+++ src/test/java/RestATBPractice/tests/RestCase.java
@@ -0,0 +1,31 @@
+package RestATBPractice.tests;
+
+import io.restassured.RestAssured;
+
+public class RestCase {
+    public static void main(String[] args) {
+
+        //Get request
+        //URL
+        //Header
+        //GetMethod
+        //base url-https://www.zippopotam.us
+        //basepath-/IN/509206
+        //Payload
+        //auth- no
+
+
+        //status code
+        //Response code
+        //Time Headers, Coockies
+
+
+        //Gerkin -->given, when, then
+        //BDD- is framework
+        RestAssured
+                .given().baseUri("https://www.zippopotam.us")
+                .basePath("/IN/560037")
+                .when().log().all().get().
+                then().log().all().statusCode(200);
+    }
+}
